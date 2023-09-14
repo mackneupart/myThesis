2eb00c153248f1d8897727c664998f8cb420ea7c
macn <macn@itu.dk>
2023-09-14 12:28:04 +0200

    imported fonts

--- Home.js
+++ Home.js
@@ -1,15 +1,41 @@
-import { Alert, Button, StyleSheet, Text, View } from "react-native";
+import { Button, StyleSheet, Text, View, Image } from "react-native";
+import { useFonts } from "expo-font";
+import AppLoading from "expo-app-loading";
 
 export default function Home({ navigation }) {
+  const [loaded] = useFonts({
+    "FiraCode-Regular": require("./assets/fonts/FiraCode-Regular.ttf"),
+    "FiraCode-Bold": require("./assets/fonts/FiraCode-Bold.ttf"),
+    "FiraCode-SemiBold": require("./assets/fonts/FiraCode-SemiBold.ttf"),
+    "KaiseiTokumin-Regular": require("./assets/fonts/KaiseiTokumin-Regular.ttf"),
+    "KaiseiTokumin-Bold": require("./assets/fonts/KaiseiTokumin-Bold.ttf"),
+  });
+
+  if (!loaded) {
+    return <AppLoading />; // Render loading UI or a placeholder
+  }
   return (
     <View style={styles.container}>
-      <Text style={styles.textHeader}>Home Screen!</Text>
-      <Text style={styles.textViews}>Text for testing</Text>
+      <Text style={styles.textHeader}>Queer{"\n"} Fortællinger</Text>
+      <Text style={styles.textViews}>
+        A digital archive of queer stories {"\n"} made for and created by fellow
+        queers
+      </Text>
       <View>
-        <Button
-          title="Go to see map"
-          onPress={() => navigation.navigate("Profile")}
+        <Image
+          source={require("./assets/icons/cph-map.png")} // Replace with the actual image path
+          style={{ width: 300, height: 300 }} // Set the desired width and height
         />
+      </View>
+      <View>
+        <Text style={styles.textViews}>
+          Check out the map without logging in
+          <Button
+            style={styles.textViews}
+            title="hrere"
+            onPress={() => navigation.navigate("Profile")}
+          />
+        </Text>
       </View>
     </View>
   );
@@ -23,8 +49,16 @@ const styles = StyleSheet.create({
     fontSize: 30,
   },
   textHeader: {
-    marginTop: 20,
-    fontSize: 30,
-    color: "blue",
+    fontFamily: "FiraCode-Bold",
+    marginTop: 40,
+    fontSize: 40,
+    fontWeight: "bold",
+    textAlign: "center",
+    color: "#8F5AFF",
+  },
+  textViews: {
+    fontFamily: "KaiseiTokumin-Regular",
+    textAlign: "center",
+    marginBottom: 20,
   },
 });
