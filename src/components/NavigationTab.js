d2318d8764fc12e15f17cbd45d2aa672ce5f42b1
macn <macn@itu.dk>
2023-09-19 16:31:39 +0200

    custom navigation bar

---
+++ src/components/NavigationTab.js
@@ -0,0 +1,86 @@
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Map from "../Map";
+import Profile from "../Profile";
+import { StyleSheet, Text, View, Image, TouchableOpacity } from "react-native";
+
+const Tab = createBottomTabNavigator();
+
+export default function NavigationTab({ handleUserLogout }) {
+  return (
+    <Tab.Navigator
+      screenOptions={{
+        tabBarShowLabel: false,
+        tabBarStyle: {
+          backgroundColor: "lightgrey",
+          position: "absolute",
+          bottom: 25,
+          left: 20,
+          right: 20,
+          elevation: 0,
+          borderRadius: 15,
+          height: 90,
+          ...style.shadow,
+        },
+      }}>
+      <Tab.Screen
+        name="Map"
+        component={Map}
+        options={{
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <View style={{ alignItems: "center", marginTop: 25 }}>
+              <Image
+                source={require("../assets/icons/map.png")}
+                resizeMode="contain"
+                style={{
+                  width: 30,
+                  height: 30,
+                  tintColor: focused ? "blue" : "grey",
+                }}
+              />
+              <Text style={{ color: focused ? "blue" : "grey", fontSize: 18 }}>
+                MAP
+              </Text>
+            </View>
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Profile"
+        options={{
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <View style={{ alignItems: "center", marginTop: 25 }}>
+              <Image
+                source={require("../assets/icons/user.png")}
+                resizeMode="contain"
+                style={{
+                  width: 30,
+                  height: 30,
+                  tintColor: focused ? "blue" : "grey",
+                }}
+              />
+              <Text style={{ color: focused ? "blue" : "grey", fontSize: 18 }}>
+                PROFILE
+              </Text>
+            </View>
+          ),
+        }}>
+        {(props) => <Profile {...props} handleUserLogout={handleUserLogout} />}
+      </Tab.Screen>
+    </Tab.Navigator>
+  );
+}
+
+const style = StyleSheet.create({
+  shadow: {
+    shadowColor: "darkgrey",
+    shadowOffset: {
+      width: 0,
+      height: 10,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.5,
+    elevation: 5,
+  },
+});
