b0b7da511c7d8137ec2d8c3c7e668015ae674e84
macn <macn@itu.dk>
2024-01-21 12:26:47 +0100

    updated sdk

--- src/config/Firebase.js
+++ src/config/Firebase.js
@@ -2,7 +2,10 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
-import { getStorage, ref } from "firebase/storage";
+import { getStorage, ref, uploadBytes } from "firebase/storage";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
